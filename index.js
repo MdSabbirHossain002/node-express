33bafca0b001f3123e760b32b143711463da0269
Md Sabbir Hossain <80336752+MdSabbirHossain002@users.noreply.github.com>
2022-04-08 19:47:53 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,13 @@ let port = process.env.PORT || 3000
 let app = require('./app')
 
 
+app.use('/',(req.res)=>{  res.send('there was an errorrrr')};
+//         
 
 
 
 
-// app.use((err, req, res, next) => {
-//     if (res.headerSent) {
-//         next('there was an problem');
-//     } else {
-//         if (err.massage) {
-//             res.status(500).send(err.massage);
-//         } else {
-//             res.send('there was an errorrrr');
-//         }
-//     }
-// });
+
 
 app.listen((port), () => {
     console.log(`server is running at port http://localhost:${3000}`);
